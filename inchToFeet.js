d5a3fda9cd70d0dda0d21333e70c340384d5d829
Tamim Mahmud <internationalinf24@gmail.com>
2022-06-16 23:32:33 +0600

    Convert inch to feet in js

---
+++ inchToFeet.js
@@ -0,0 +1,16 @@
+// var inch = 156;
+// var feet = inch/12;
+// console.log(feet);
+// console.log(feet);
+// console.log(feet);
+
+function inchToFeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var vaifeet = inchToFeet(156); 
+console.log(vaifeet);
+var borovaifeet = inchToFeet(144); 
+console.log(borovaifeet);
+var sotovaifeet = inchToFeet(132); 
+console.log(sotovaifeet);
